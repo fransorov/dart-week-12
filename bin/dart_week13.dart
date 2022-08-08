4568099816e75657f73e7dfc6c0df65c742f8d1a
fransorov <franforker@gmail.com>
2022-08-08 11:32:52 +0700

    first commit

---
+++ bin/dart_week13.dart
@@ -0,0 +1,7 @@
+void main() {
+  int i = 1 ;
+  while (i <= 24) {
+    print('24x$i = ${i*24}');
+    i = i + 1;
+  }
+}
---
+++ test/dowhile.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  
+  int i = 1;
+
+  do {
+    print(i);
+    i = i + 1;
+  } while (i <= 4);
+
+}
